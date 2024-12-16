e7540a7e6d19811a3052af330e683cc047aca326
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:16:06 +0530

    Update maintenance.py

--- MoonXMusic/plugins/sudo/maintenance.py
+++ MoonXMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
