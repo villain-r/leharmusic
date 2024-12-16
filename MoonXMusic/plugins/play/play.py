0528f944d531ee60170d7e5fbe51b4c776926f2c
newiseland <herukiraj@gmail.com>
2024-12-16 21:13:31 +0530

    ᴍᴏᴏɴ

--- MoonXMusic/plugins/play/play.py
+++ MoonXMusic/plugins/play/play.py
@@ -6,22 +6,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AnonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import seconds_to_min, time_to_seconds
-from AnonXMusic.utils.channelplay import get_channeplayCB
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.decorators.play import PlayWrapper
-from AnonXMusic.utils.formatters import formats
-from AnonXMusic.utils.inline import (
+from MoonXMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from MoonXMusic.core.call import Moony
+from MoonXMusic.utils import seconds_to_min, time_to_seconds
+from MoonXMusic.utils.channelplay import get_channeplayCB
+from MoonXMusic.utils.decorators.language import languageCB
+from MoonXMusic.utils.decorators.play import PlayWrapper
+from MoonXMusic.utils.formatters import formats
+from MoonXMusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AnonXMusic.utils.logger import play_logs
-from AnonXMusic.utils.stream.stream import stream
+from MoonXMusic.utils.logger import play_logs
+from MoonXMusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 
