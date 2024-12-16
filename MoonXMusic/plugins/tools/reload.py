251cd81c208573c603a89b90f3c408bd4ce52dc9
newiseland <herukiraj@gmail.com>
2024-12-16 21:30:43 +0530

    ᴍᴏᴏɴ

--- MoonXMusic/plugins/tools/reload.py
+++ MoonXMusic/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from MoonXMusic import app
+from MoonXMusic.core.call import Moony
+from MoonXMusic.misc import db
+from MoonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
+from MoonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
+from MoonXMusic.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
