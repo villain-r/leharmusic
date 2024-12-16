7d9c17125830333f25283d480b6f2faa9b5e65d0
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:48:20 +0530

    Update play.py

--- MoonXMusic/utils/decorators/play.py
+++ MoonXMusic/utils/decorators/play.py
@@ -9,9 +9,9 @@ from pyrogram.errors import (
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from MoonXMusic import YouTube, app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import (
     get_assistant,
     get_cmode,
     get_lang,
@@ -20,7 +20,7 @@ from AnonXMusic.utils.database import (
     is_active_chat,
     is_maintenance,
 )
-from AnonXMusic.utils.inline import botplaylist_markup
+from MoonXMusic.utils.inline import botplaylist_markup
 from config import PLAYLIST_IMG_URL, SUPPORT_CHAT, adminlist
 from strings import get_string
 
