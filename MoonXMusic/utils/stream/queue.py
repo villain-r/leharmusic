2e8f236e24376680f1470d70b7796ac591007525
newiseland <herukiraj@gmail.com>
2024-12-16 22:56:30 +0530

    Update queue.py

--- MoonXMusic/utils/stream/queue.py
+++ MoonXMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from AnonXMusic.misc import db
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min
+from MoonXMusic.misc import db
+from MoonXMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
