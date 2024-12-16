ba81d51485cd34b671b1a481d520ff7d0dc82081
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:30:03 +0530

    Update admins.py

--- MoonXMusic/utils/decorators/admins.py
+++ MoonXMusic/utils/decorators/admins.py
@@ -1,9 +1,9 @@
 from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS, db
+from MoonXMusic.utils.database import (
     get_authuser_names,
     get_cmode,
     get_lang,
