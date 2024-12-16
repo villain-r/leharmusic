92150a7c4bfe507875221f769bdf017ac3f86952
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:26:51 +0530

    Update sudoers.py

--- MoonXMusic/plugins/sudo/sudoers.py
+++ MoonXMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import add_sudo, remove_sudo
+from MoonXMusic.utils.decorators.language import language
+from MoonXMusic.utils.extraction import extract_user
+from MoonXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
