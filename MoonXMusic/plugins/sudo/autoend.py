0211c4fbed11bdf65d5e7b325807d0a79c82b7ef
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:09:29 +0530

    Update autoend.py

--- MoonXMusic/plugins/sudo/autoend.py
+++ MoonXMusic/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import autoend_off,autoend_on,autoleave_off, autoleave_on,is_autoend,is_autoleave
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import autoend_off,autoend_on,autoleave_off, autoleave_on,is_autoend,is_autoleave
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
