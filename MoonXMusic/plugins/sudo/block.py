c0bcdfefce72ed662d464e1a832989dd358efff5
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 21:12:20 +0530

    Update block.py

--- MoonXMusic/plugins/sudo/block.py
+++ MoonXMusic/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_gban_user, remove_gban_user
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import add_gban_user, remove_gban_user
+from MoonXMusic.utils.decorators.language import language
+from MoonXMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
