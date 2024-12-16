fdcc5306f964820a655d68fdb40cabb401911233
newiseland <herukiraj@gmail.com>
2024-12-16 21:28:15 +0530

    Update queue.py

--- MoonXMusic/plugins/tools/queue.py
+++ MoonXMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AnonyBin, get_channeplayCB, seconds_to_min
-from AnonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline import queue_back_markup, queue_markup
+from MoonXMusic import app
+from MoonXMusic.misc import db
+from MoonXMusic.utils import MoonyBin, get_channeplayCB, seconds_to_min
+from MoonXMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from MoonXMusic.utils.decorators.language import language, languageCB
+from MoonXMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
