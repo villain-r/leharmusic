28994b28d75e637954a6c5193dd78fc7a2fab890
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-16 20:35:30 +0530

    Update bot.py

--- MoonXMusic/core/bot.py
+++ MoonXMusic/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Moon(Client):
+class Moony(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
