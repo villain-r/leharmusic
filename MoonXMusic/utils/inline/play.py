f331bb09448478057763c1976bbebd44ed9c2e0e
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-18 16:43:52 +0530

    Moon 🌙

--- MoonXMusic/utils/inline/play.py
+++ MoonXMusic/utils/inline/play.py
@@ -83,11 +83,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"MoonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"MoonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
