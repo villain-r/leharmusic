af7a86eaee4fa73447e16fd9e4d0a5ce2413b944
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-18 20:45:26 +0530

    Update Youtube.py

--- MoonXMusic/platforms/Youtube.py
+++ MoonXMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from MoonXMusic.utils.database import is_on_off
+from MoonXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
