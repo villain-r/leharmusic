7f6bd913168c2c825afdfa9daef6e30558694d22
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-19 22:51:44 +0530

    Moon 🌙

--- MoonXMusic/plugins/tools/speedtest.py
+++ MoonXMusic/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from RessoMusic import app
-from RessoMusic.misc import SUDOERS
-from RessoMusic.utils.decorators.language import language
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.decorators.language import language
 
 
 def testspeed(m, _):
