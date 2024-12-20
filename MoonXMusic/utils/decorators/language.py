603d8ac403a7e4b83b460350a607e79eded20387
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-20 08:14:25 +0530

    Update language.py

--- MoonXMusic/utils/decorators/language.py
+++ MoonXMusic/utils/decorators/language.py
@@ -1,7 +1,7 @@
-from MoonXMusic.misc import SUDOERS
-from MoonXMusic.utils.database import get_lang, is_maintenance
-from config import SUPPORT_GROUP
 from strings import get_string
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import (get_lang, is_commanddelete_on,
+                                       is_maintenance)
 
 
 def language(mystic):
@@ -9,14 +9,13 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_GROUP}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
-                    disable_web_page_preview=True,
+                    "Bot is under maintenance. Please wait for some time..."
                 )
-        try:
-            await message.delete()
-        except:
-            pass
-
+        if await is_commanddelete_on(message.chat.id):
+            try:
+                await message.delete()
+            except:
+                pass
         try:
             language = await get_lang(message.chat.id)
             language = get_string(language)
@@ -32,7 +31,7 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    "Bot is under maintenance. Please wait for some time...",
                     show_alert=True,
                 )
         try:
