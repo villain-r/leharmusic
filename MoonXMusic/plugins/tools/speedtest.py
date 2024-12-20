25687c6666622df76f73ca3c2bfcd7adb8aa2fe0
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-20 11:55:05 +0530

    Update speedtest.py

--- MoonXMusic/plugins/tools/speedtest.py
+++ MoonXMusic/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from MoonXMusic import app
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.decorators.language import language
 
 
 def testspeed(m, _):
