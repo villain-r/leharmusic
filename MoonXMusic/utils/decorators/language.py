3a9f39aea74d8530fdbf260cac7e4186d4208448
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-12-20 11:01:26 +0530

    Update language.py

--- MoonXMusic/utils/decorators/language.py
+++ MoonXMusic/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from MOON.misc import SUDOERS
-from MOON.utils.database import get_lang, is_maintenance
+from MoonXMusic.misc import SUDOERS
+from MoonXMusic.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
