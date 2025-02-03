1485cf8472189def4e3d33ed285d8b578c6ce059
villain-r <villainji96@gmail.com>
2025-02-03 18:24:36 +0530

    Update play.py

--- MoonXMusic/utils/inline/play.py
+++ MoonXMusic/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from MoonXMusic.utils.formatters import time_to_seconds
+from vexaaXMusic.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
