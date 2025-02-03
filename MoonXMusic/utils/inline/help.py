80daabf9e506495e08117a34af25443c5d44aaeb
villain-r <villainji96@gmail.com>
2025-02-03 18:23:25 +0530

    Update help.py

--- MoonXMusic/utils/inline/help.py
+++ MoonXMusic/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from MoonXMusic import app
+from vexaaXMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
